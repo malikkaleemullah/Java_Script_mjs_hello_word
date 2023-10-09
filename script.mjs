90e11c9ade54da39ba33576ca8c89c03753bd1c5
Kaleem Ullah <malikkaleemullah2k@gmail.com>
2023-10-09 17:23:02 +0500

    staring es6

--- script.mjs
+++ script.mjs
@@ -1,1 +1,8 @@
 console.log ("hello from mjs file");
+let myNumber = 10;
+let myName = "Malik Kaleem"
+console.log (myNumber, myName)
+
+myNumber = 40
+myName = "Malik Kaleem Ullah"
+console.log (myNumber, myName)
